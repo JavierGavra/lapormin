c5d57e376c3666d4a0c1edf4238037e688652351
Lenovo Gavra <gavrajavier01@gmail.com>
2026-05-23 17:23:17 +0700

    feat: [JG] made UI of step 3 for create_report_page

--- lib/features/report/presentation/widgets/create_report/evidences_step.dart
+++ lib/features/report/presentation/widgets/create_report/evidences_step.dart
@@ -1,10 +1,173 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:lapormin/core/widgets/snackbar/custom_snackbar.dart';
+import '../../../../../core/utils/text_style/app_text_style.dart';
+import 'create_report_step_header.dart';
 
 class EvidencesStep extends StatelessWidget {
   const EvidencesStep({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final color = Theme.of(context).colorScheme;
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 24,
+        children: [
+          const CreateReportStepHeader(
+            title: "Tambahkan bukti\n(Foto/Video)",
+            description: "Buktikan bahwa laporanmu valid.",
+          ),
+          _buildUploadButton(color, context),
+          _buildPreview(color),
+          const SizedBox(height: 0),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildUploadButton(ColorScheme color, BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: color.primary,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            showSnackBar(context, "Comming Soon!");
+          },
+          borderRadius: BorderRadius.circular(12),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 36),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              spacing: 12,
+              children: [
+                Icon(
+                  Icons.camera_alt_rounded,
+                  color: color.onPrimary,
+                  size: 32,
+                ),
+                Text(
+                  "Ambil Foto/Video",
+                  textAlign: TextAlign.center,
+                  style: AppTextStyle.s14(
+                    color: color.onPrimary,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPreview(ColorScheme color) {
+    const int crossAxisCount = 3;
+    const double gap = 8;
+    const double containerPadding = 16;
+
+    return DottedBorder(
+      options: RoundedRectDottedBorderOptions(
+        radius: Radius.circular(12),
+        padding: EdgeInsets.zero,
+        color: color.outline,
+        dashPattern: [5, 5],
+        strokeWidth: 1.5,
+      ),
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(containerPadding),
+        decoration: BoxDecoration(
+          color: color.surfaceContainerHighest,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          spacing: 12,
+          children: [
+            Text(
+              "Terpilih : 4/5",
+              style: AppTextStyle.s14(
+                color: color.onSurfaceVariant,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            LayoutBuilder(
+              builder: (context, constraints) {
+                // Hitung lebar tiap gambar secara dinamis
+                // Total gap antar item = (crossAxisCount - 1) * gap
+                final itemWidth =
+                    (constraints.maxWidth - (gap * (crossAxisCount - 1))) /
+                    crossAxisCount;
+
+                return Wrap(
+                  spacing: gap,
+                  runSpacing: gap,
+                  children: List.generate(
+                    4,
+                    (index) => SizedBox(
+                      width: itemWidth,
+                      height: itemWidth, // biar tetap square
+                      child: Stack(
+                        clipBehavior: Clip.none,
+                        children: [
+                          // Gambar
+                          Positioned.fill(
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: color.primaryContainer,
+                                borderRadius: BorderRadius.circular(8),
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    "assets/images/cards/infrastruktur.png",
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                          // Tombol X di pojok kanan atas
+                          // Positioned(
+                          //   // top: -8,
+                          //   // right: -8,
+                          //   child: IconButton.filled(
+                          //     onPressed: () {},
+                          //     icon: Icon(
+                          //       Icons.close_rounded,
+                          //       size: 16,
+                          //       color: color.tertiary,
+                          //     ),
+                          //     padding: EdgeInsets.zero,
+                          //     constraints: const BoxConstraints(
+                          //       minWidth: 28,
+                          //       minHeight: 28,
+                          //     ),
+                          //     style: IconButton.styleFrom(
+                          //       shape: const CircleBorder(),
+                          //       backgroundColor: color.surface,
+                          //       padding: EdgeInsets.zero,
+                          //     ),
+                          //   ),
+                          // ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
